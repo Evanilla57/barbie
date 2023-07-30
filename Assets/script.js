ae659f9f7d3163ff834b860a53b92f7ed4cae90a
Evanilla57 <grigg005@cougars.csusm.edu>
2023-07-30 12:37:04 -0700

    added variable for todaay's date, added dayjs function and displayed date on web page

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,10 @@
+var todayDate = dayjs();
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(document).ready(function () {
-
+  $('#currentDay').text(todayDate.format('MMM D, YYYY'));
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
