5217b90c9e118b99c8dcb30e9b6c2c1e73d39bd2
Evanilla57 <grigg005@cougars.csusm.edu>
2023-07-30 12:49:41 -0700

    added event listener targetting id for unique hour  within each div

--- Assets/script.js
+++ Assets/script.js
@@ -6,13 +6,19 @@ var todayDate = dayjs();
 $(document).ready(function () {
   $('#currentDay').text(todayDate.format('MMM D, YYYY'));
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+
+  $('.saveBtn').on('click', function () {
+    var hour = $(this).parent().attr('id');
+    localStorage.setItem('hour', )
+  })
+    // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
